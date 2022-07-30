60ec83cc9eb844741013297b0f19884c27ccf202
Ashish <65858601+Ashish-AVS@users.noreply.github.com>
2022-07-30 23:27:44 +0530

    Update Login.js

--- src/views/Login.js
+++ src/views/Login.js
@@ -34,14 +34,14 @@ export default function Login() {
               <div>
                 <label
                   for="password"
-                     type="password"
-                    required
                   class="inline-block text-gray-800 text-sm sm:text-base mb-2"
                 >
                   Password
                 </label>
                 <input
                   name="password"
+type="password"
+                    required
                   class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
                 />
               </div>
