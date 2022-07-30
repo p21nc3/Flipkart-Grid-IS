e40a7c18e83ae706b13e6c4393105704c3a5e20b
avsashish9@gmail.com <=>
2022-07-31 00:10:22 +0530

    Fix Bug

--- src/components/Register.js
+++ src/components/Register.js
@@ -25,18 +25,7 @@ export default function Register(props) {
 
           <form class="max-w-lg border rounded-lg mx-auto" onSubmit={submitHandler}>
             <div class="flex flex-col gap-4 p-4 md:p-8">
-              <div>
-                <label
-                  for="email"
-                  class="inline-block text-gray-800 text-sm sm:text-base mb-2"
-                >
-                  {data.first}
-                </label>
-                <input
-                  name="email"
-                  class="w-full bg-gray-50 text-gray-800 border focus:ring ring-indigo-300 rounded outline-none transition duration-100 px-3 py-2"
-                />
-              </div>
+              
               
               <div>
                 <label
